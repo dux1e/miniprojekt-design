1251b1af0ff752e4107b6d70b75d1fcf778cbc6b
joakim <joak0451@gmail.com>
2020-11-02 10:43:23 +0100

    Initial sharing of project

---
+++ controller/Ngg.java
@@ -0,0 +1,27 @@
+package controller;
+
+/**
+ * Write a description of class Ngg here.
+ *
+ * @author Joakim Christensen
+ * @version (a version number or a date)
+ */
+public class Ngg {
+    // Instance Variables
+
+    /**
+     * Construtor for Ngg.
+     */
+    public Ngg() {
+
+    }
+
+    // other methods
+
+    // getter and setter
+    public void setName(String name) { 
+
+        this.name = name; 
+
+    } 
+}
---
+++ tui/MainMenuUI.java
@@ -0,0 +1,24 @@
+package tui;
+
+
+/**
+* Write a description of class MainMenuUI here.
+*
+* @author Joakim Christensen
+* @version (a version number or a date)
+*/
+public class MainMenuUI {
+    // Instance Variables
+
+    /**
+     * Construtor for MainMenuUI.
+     */
+    public MainMenuUI() {
+
+    }
+
+    // other methods
+
+    // getter and setter
+
+}
